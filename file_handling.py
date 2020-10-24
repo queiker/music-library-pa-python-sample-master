8ff147776262f88d6b46f33cc64e8bcd42e7fe1d
Grzegorz Płonka <tradecomp.pl@gmail.com>
2020-10-24 19:32:30 +0200

    gotowe funkcje importu i exportu do pliku

--- file_handling.py
+++ file_handling.py
@@ -18,11 +18,29 @@ def import_data(filename='albums_data.txt'):
 
     for i in range(0, len(read_data)):
         read_data[i].split(",")
+    print(read_data)
+    #poniższy fragment kodu dzieli utwory na pojedyńcze treści z fragmentów:
+    #'Pink Floyd,The Dark Side Of The Moon,1973,progressive rock,43:00' robi :
+    #"Britney Spears", "Baby One More Time", "1999", "pop", "42:20"
+
+    for i in range(0,len(read_data)):
+        line1 = str(read_data[i])
+        line1 = line1.split(',')
+        read_data[i] = line1
+        # if len(read_data[i]) == 5:
+        #     read_data[i][0] = str(read_data[i][0])
+        #     read_data[i][1] = str(read_data[i][1])
+        #     read_data[i][2] = int(read_data[i][2])
+        #     read_data[i][3] = str(read_data[i][3])
+        #     read_data[i][4] = str(read_data[i][4])
+    
+    
+    print(read_data)
 
     #poniżej jest kod kasujący puste linie które w tabeli były by jako ''
     lines_do_delete = []
     for i in range(0,len(read_data)):
-        if read_data[i] == '':
+        if read_data[i] == ['']:
             lines_do_delete.append(i)
 
     for i in range(len(lines_do_delete), 0, -1):
@@ -33,6 +51,8 @@ def import_data(filename='albums_data.txt'):
 
 #Poniżej kod testujący funkcję import_data
 #print(import_data('albums_data.txt') )
+
+#import_data('albums_data.txt')
 
 def export_data(albums, filename='albums_data.txt', mode='a'):
     """
@@ -55,15 +75,24 @@ def export_data(albums, filename='albums_data.txt', mode='a'):
             existing_albums.append(albums[iterate])
         albums = existing_albums
 
-    elif mode == "w":
-        pass
+        with open(filename, 'w') as line:
+            for i in range(0,len(albums)):
+                if len(albums[i]) == 5:
+                    line.write(",".join(albums[i]) + "\n")
+
+    elif mode == 'w':
+        with open(filename, 'w') as line:
+            for i in range(0,len(albums)):
+                if len(albums[i]) == 5:
+                    line.write(",".join(albums[i]) + "\n")
+                    
     else:
         raise ValueError
     
 
-    print(albums)
+    #print(albums)
 
 
-print(export_data([["YYYYYYYYYYYYYYYYYYYYYYYY", "Baby One More Time", "1999", "pop", "42:20"],["XXXXXXXXXXXXXXXXX", "Low", "1977", "rock", "38:26"] ], filename='albums_data.txt', mode='a') )
+#print(export_data([["YYYYYYYYYYYYYYYYYYYYYYYY", "Baby One More Time", "1999", "pop", "42:20"],["XXXXXXXXXXXXXXXXX", "Low", "1977", "rock", "38:26"] ], filename='albums_data.txt', mode='a') )
 
-
+#print(export_data([["HIHI HEY HEY", "Baby One More Time", "1999", "pop", "42:20"]], filename='albums_data.txt', mode='a') )
