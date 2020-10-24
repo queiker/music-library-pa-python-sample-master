912eda432003c2262fd283d6e5afee4aea99c6bc
Grzegorz Płonka <tradecomp.pl@gmail.com>
2020-10-24 15:17:08 +0200

    first commit branch development

---
+++ file_handling.py
@@ -0,0 +1,50 @@
+def import_data(filename='albums_data.txt'):
+    """
+    Import data from a file to a list. Expected returned data format:
+        ["David Bowie", "Low", "1977", "rock", "38:26"],
+        ["Britney Spears", "Baby One More Time", "1999", "pop", "42:20"],
+        ...]
+
+    :param str filename: optional, name of the file to be imported
+
+    :returns: list of lists representing albums' data
+    :rtype: list
+    """
+    with open(filename) as f:
+        read_data = f.read()
+    
+    read_data = read_data.split("\n")
+    #print(read_data)
+
+    for i in range(0, len(read_data)):
+        read_data[i].split(",")
+
+    #poniżej jest kod kasujący puste linie które w tabeli były by jako ''
+    lines_do_delete = []
+    for i in range(0,len(read_data)):
+        if read_data[i] == '':
+            lines_do_delete.append(i)
+
+    for i in range(len(lines_do_delete), 0, -1):
+        read_data.pop(lines_do_delete[i-1])
+
+    return read_data
+        
+
+
+import_data('albums_data.txt')
+
+def export_data(albums, filename='albums_data.txt', mode='a'):
+    """
+    Export data from a list to file. If called with mode 'w' it should overwrite
+    data in file. If called with mode 'a' it should append data at the end.
+
+    :param list albums: albums' data
+    :param str filename: optional, name of file to export data to
+    :param str mode: optional, file open mode with the same meaning as\
+    file open modes used in Python. Possible values: only 'w' or 'a'
+
+    :raises ValueError: if mode other than 'w' or 'a' was given. Error message:
+        'Wrong write mode'
+    """
+
